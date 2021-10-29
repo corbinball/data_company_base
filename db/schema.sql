fb69808398c0a270fa0bfc8029a230ef67033ce1
Corbin Ball <c.ball@me.com>
2021-10-29 13:34:24 -0500

    completed roles table

--- db/schema.sql
+++ db/schema.sql
@@ -3,14 +3,17 @@ CREATE DATABASE theworkplace_db;
 
 USE theworkplace_db;
 
-CREATE TABLE departments (
-    id INT NOT NULL PRIMARY KEY,
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-
-
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8,2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employess (
