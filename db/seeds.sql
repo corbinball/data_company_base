2b7f2321adc7671b0475cd6f7b9d7d2e01bf9ed6
Corbin Ball <c.ball@me.com>
2021-10-29 14:14:43 -0500

    updated with npm i

--- db/seeds.sql
+++ db/seeds.sql
@@ -20,4 +20,4 @@ INSERT INTO employees (first_name, last_name, roles_id, manager_id)
 VALUES ("Michael", "Scott", 1, 1),
        ("Dwight", "Schrute", 2, NULL),
        ("Kevin", "Malone", 3, NULL),
-       ("Darryl", "Philbin", 4, NULL),
+       ("Darryl", "Philbin", 4, NULL);
