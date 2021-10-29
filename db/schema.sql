e82b3ae28985ad0ef772a657b0a16bf92569303e
Corbin Ball <c.ball@me.com>
2021-10-29 13:08:33 -0500

    created tables for departments, roles, and employees

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,18 @@
 DROP DATABASE IF EXISTS theworkplace_db;
 CREATE DATABASE theworkplace_db;
+
+USE theworkplace_db;
+
+CREATE TABLE departments (
+
+);
+
+CREATE TABLE roles (
+
+
+);
+
+CREATE TABLE employess (
+
+
+);
