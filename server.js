eb04503368774e574cb4177caefd2102e45e2ba0
Corbin Ball <c.ball@me.com>
2021-10-29 15:24:44 -0500

    connect to db

--- server.js
+++ server.js
@@ -2,3 +2,14 @@
 const  inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+
+// connect to db
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'Bootcamp12!34$',
+      database: 'theworkplace_db'
+    },
+    console.log(`Connected to the theworkplace_db.`)
+  );
